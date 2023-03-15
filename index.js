6f508a2825b71311da59131531ff29a0005b0321
KeithPetr <keith.petryshyn@gmail.com>
2023-03-14 22:38:09 -0400

    Able to use the titleArray
    Can display the results of the search, still need proper formatting

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 const apiKey = "33db42b7";
 const searchBtn = document.getElementById("search-button");
 const searchInput = document.getElementById("search-bar");
+const movieResultsEl = document.getElementById("movie-results")
 
-
+let titleArray = [];
 
 // --------------------- Functions -----------------------------
 
 function search() {
-    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput.value}`)
-      .then((res) => res.json())
-      .then((data) => {
-        getTitlesArray(data.Search)
+  fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      getTitlesArray(data.Search);
+      getMovieHtml(titleArray);
     });
-  }
-
-function getTitlesArray(data) {
-    for (let movie of data) {
-        console.log(movie.Title)
-    }
 }
 
-function getMovieHtml(data) {
-    return `
-    <div class="movie-container">
-        <img src='${data}' alt="movie poster for ${data}">
-        <div class="movie-info">
-            <div class="title-rating">
-                <h3 class="title"></h3>
-                <p class="rating">Star ${data}</p>
-            </div>
-            <div class="time-genre-watchlist">
-                <p class="time">${data}</p>
-                <p class="genre">${data}</p>
-                <p id="add-to-list" class="watchlist">Plus ${data}</p>
-            </div>
-            <p class="plot">${data}</p>
-        </div>
-    </div>
-    `
+function getTitlesArray(data) {
+  for (let movie of data) {
+    titleArray.push(movie.Title);
   }
+  console.log(titleArray);
+}
+
+function getMovieHtml() {
+  for (let title of titleArray) {
+    fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`)
+      .then((res) => res.json())
+      .then((data) => {
+        movieResultsEl.innerHTML += `<div class="movie-container">
+            <img src='${data.Poster}' alt="movie poster for ${data.Title}">
+            <div class="movie-info">
+                <div class="title-rating">
+                    <h3 class="title">${data.Title}</h3>
+                    <p class="rating">Star ${data.imdbRating}</p>
+                </div>
+                <div class="time-genre-watchlist">
+                    <p class="time">${data.Runtime}</p>
+                    <p class="genre">${data.Genre}</p>
+                    <p id="add-to-list" class="watchlist">Plus</p>
+                </div>
+                <p class="plot">${data.Plot}</p>
+            </div>
+        </div>`;
+      });
+  }
+}
 
 // ------------------------ Event Listeners --------------------
 searchBtn.addEventListener("click", search);
